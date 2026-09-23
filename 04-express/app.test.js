62851e695ff461adf4fd6e344aec39a5833aaa1b
Alex-Lett <bernier.alejandro99@gmail.com>
2026-09-23 12:19:01 -0300

    23 sept.2

--- 04-express/app.test.js
+++ 04-express/app.test.js
@@ -1,6 +1,6 @@
 import { test, describe, before, after } from 'node:test'
 import assert from 'node:assert'
-import { app } from './app.js'
+import  app  from './app.js'
 
 let server
 const PORT = 3456
